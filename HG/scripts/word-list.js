33b0391f79e07eef3bd3278927f7c7f21bd87e6e
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2023-12-17 20:08:56 +0700

    Create word-list.js

---
+++ HG/scripts/word-list.js
@@ -0,0 +1,13 @@
+const wordList = [
+    {
+        word: "notch",
+        hint: "A Developer Minecraft."
+    },
+  {
+        word: "technoblade",
+        hint: "....."Never Dies".."
+    },
+  {
+        word: "pinksheep",
+        hint: "A Rare Pink Mob in Minecraft."
+  }
