966f867042130ea7722eb69235b9813faae8491d
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2023-12-20 05:29:52 +0700

    Update main.js

--- GPT/main.js
+++ GPT/main.js
@@ -2,10 +2,16 @@ function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
+    let userValue = "user";
+    let passValue = "password";
     // Contoh: Validasi username dan password (gunakan yang lebih aman dalam implementasi yang sebenarnya)
-    if (username === "user" && password === "pass") {
-        alert("Login berhasil!");
+    if (username === userValue; && password === passValue;) {
+        berhasil();
     } else {
         alert("Login gagal. Coba lagi.");
     }
+}
+
+function berhasil() {
+    open.window("https://alvin132qr/main/main.html")
 }
