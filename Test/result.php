ef4e3494dd7c6032450ee14d395d7a82861e4bfb
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2024-01-08 14:51:41 +0700

    Update result.php

--- Test/result.php
+++ Test/result.php
@@ -4,6 +4,7 @@
 <title>Forms action page</title>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body class="w3-container">
+  <a href="https://www.w3schools.com/action_page.php" download>Downloadf</a>
 <h1>Submitted Form Data</h1>
 <h2>Your input was received as:</h2>
 <div class="w3-container w3-large w3-border" style="word-wrap:break-word">
