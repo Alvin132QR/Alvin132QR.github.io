88afc79af75e2b49651678f0a7c7d00f7d336d79
Alvin132QR <114194093+Alvin132QR@users.noreply.github.com>
2024-01-28 14:44:12 +0700

    Update script.js

--- GPT/script.js
+++ GPT/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
